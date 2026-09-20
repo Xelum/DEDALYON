a20bd646f4f14d0be87aa0a20f182a455ee465c7
Xelum <alex.morneala@libero.it>
2026-09-20 22:50:36 +0200

    Delete forgefall.js

--- forgefall.js
+++
@@ -1,9 +0,0 @@
-/* Compatibilità con la vecchia home: Forgefall ora vive in una pagina autonoma. */
-(() => {
-  'use strict';
-  const GAME_URL = './giochi/forgefall/index.html';
-  window.Forgefall = {
-    open() { window.location.href = GAME_URL; },
-    reset() { window.location.href = GAME_URL; }
-  };
-})();
